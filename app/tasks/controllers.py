485b5cf686a5bb573ecfed598551cf15437e1d5d
Erik-CR46 <erikcatalanrodriguez@gmail.com>
2026-07-25 22:50:29 +0200

    templates, jinja2, create, update

--- app/tasks/controllers.py
+++ app/tasks/controllers.py
@@ -1,23 +1,27 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request
 
 taskRoute = Blueprint('tasks', __name__, url_prefix='/tasks')
 
-#@taskRoute.route('/')
-#def index():
-#    return "Index"
+task_list = [1,2,3]
 
-@taskRoute.route('/<int:id>')
-def show(id:int):
-    return 'Show ' + str(id)
-
-@taskRoute.route('/delete/<int:id>')
-def delete(id:int):
-    return 'Delete ' + str(id)
+@taskRoute.route('/')
+def index():
+    return render_template("tasks/index.html", task_list= task_list)
 
 @taskRoute.route('/create', methods=('GET', 'POST'))
 def create():
-    return 'Create'
+    task = request.form.get('task')
+    if task is not None:
+        task_list.append(task)
+    return render_template("tasks/create.html")
+
+@taskRoute.route('/delete/<int:id>')
+def delete(id:int):
+    return 'Delete ' + str(id) 
 
 @taskRoute.route('/update/<int:id>', methods=['GET', 'POST'])
 def update(id:int):
-    return 'update' + str(id)
+    task = request.form.get('task')
+    if id >= 0 or id <= len(task_list):
+        task_list[id] = task
+    return render_template("tasks/update.html")
