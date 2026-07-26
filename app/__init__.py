b4a732d6f95453a51a9829c806c6e2a8725041bb
Erik-CR46 <erikcatalanrodriguez@gmail.com>
2026-07-26 20:27:52 +0200

    Clases basadas en vistas (Sustituyo las funciones de controlers por views con clases)

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,7 @@
 from flask import Flask, render_template, request
 from app.config import DevelopmentConfig
-from app.tasks.controllers import taskRoute
 
 app = Flask(__name__)
-app.register_blueprint(taskRoute)
-
 app.config.from_object(DevelopmentConfig)
 
-
-@app.route('/')
-def main():
-    name = request.args.get('name', 'Desarrollador')
-    return render_template('index.html', name=name)
+from app.tasks import views
